ce3a0449064216d5bdad2801649d41feb4643314
Doug Jones <dsjr1284@gmail.com>
2020-04-03 20:50:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -108,7 +108,7 @@ class TicTacToe
       turn
     end
     if won?
-     puts "Congratulations #{winner(board)}!"
+     puts "Congratulations #{winner}!"
    elsif draw?
      puts "Draw Game"
    end
