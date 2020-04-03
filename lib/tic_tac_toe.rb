26e75050ba0d312df805bfad48b7dda64f646582
Doug Jones <dsjr1284@gmail.com>
2020-04-03 18:20:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,9 +34,9 @@ class TicTacToe
       @board[index] = token
     end
 
-    # def position_taken?(board, index)
-    #   !(board [index] == " ")
-    # end
+    def position_taken?(board, index)
+      !(board [index] == " ")
+    end
 #
 #     def valid_move?(board, index)
 #       index.between?(0,8) && ! position_taken?(board,index)
